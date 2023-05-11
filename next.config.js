cac079c45b789c0e864ba553f8766aa6553a182a
code-vj <architect.vijay1990@gmail.com>
2023-05-11 16:22:49 +0530

    code cleanup & fix next build issues

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    runtime: 'edge',
+  },
+  distDir: 'build',
   swcMinify: true,
   webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.experiments = { ...config.experiments, topLevelAwait: true ,asyncWebAssembly: true};
     return config;
   },
 };
